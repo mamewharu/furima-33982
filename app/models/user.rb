f6fb8cf1674985f13462fcd5a931877dbb38952d
mamewharu <mame0040007@gmail.com>
2021-02-12 20:31:47 +0900

    修正１

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
-         validates :birth_day, presence:true
+         with_options presence:true do
+           validates :nickname
+           validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, messasge:"Last name Full-width characters"}
+           validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, messasge:"First name Full-width characters"}
+           validates :last_name_kana, format: {with: /\A[ァ-ヶ]+\z/, messasge:"Last name kana Full-width katakana characters"}
+           validates :first_name_kana, format: {with: /\A[ァ-ヶ]+\z/, messasge:"Last name kana Full-width katakana characters"}
+           validates :birth_day
+         end
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+          validates_format_of :password, with: PASSWORD_REGEX, message: "Include both letters and numbers" 
 end
