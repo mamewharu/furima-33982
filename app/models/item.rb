e3615169d2928c6b3a6823ac7c38f614a2eb1dcc
mamewharu <mame0040007@gmail.com>
2021-02-13 16:43:17 +0900

    バリデーション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  with_options presence:true do
+    validates  :name                       
+    validates  :text                      
+    validates  :category_id                
+    validates  :state_id                   
+    validates  :shipping_fee_burden_id    
+    validates  :area_id                
+    validates  :shipping_day_id            
+    validates  :price, between: {300,9999999} 
+  end
 end
