2025a9fad6b46e0b51e6af533b6cddc4f45f9979
mamewharu <mame0040007@gmail.com>
2021-02-15 12:48:47 +0900

    activehashの導入　model,validate,association記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_genre
   belongs_to :user
   has_one_attached :image
 
   with_options presence:true do
     validates  :name                       
     validates  :text                      
-    validates  :category_id                
+    validates  :category_id ,  numbericality: { other_than: 1 }
     validates  :state_id                   
     validates  :shipping_fee_burden_id    
     validates  :area_id                
