009ab60fc481d832fc64381a2e42268206e4ee81
mamewharu <mame0040007@gmail.com>
2021-02-16 17:33:45 +0900

    rubocop実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,13 +15,12 @@ class Item < ApplicationRecord
     validates  :price,
                numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
     validates  :image
-   with_options  numericality: {other_than: 1, message: 'Select'} do
-     validates  :category_id
-     validates  :state_id
-     validates  :shipping_fee_burden_id
-     validates  :area_id
-     validates  :shipping_day_id
+    with_options numericality: { other_than: 1, message: 'Select' } do
+      validates :category_id
+      validates  :state_id
+      validates  :shipping_fee_burden_id
+      validates  :area_id
+      validates  :shipping_day_id
     end
   end
 end
-
