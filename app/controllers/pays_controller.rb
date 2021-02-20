a144575251cfb08d8909e8b4809c8e0279899d17
mamewharu <mame0040007@gmail.com>
2021-02-20 11:33:44 +0900

    エラーメッセージの実装解決

--- app/controllers/pays_controller.rb
+++ app/controllers/pays_controller.rb
@@ -8,11 +8,11 @@ class PaysController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @pay_streetaddress = PayStreetAddress.new(pay_params)
-   binding.pry
+    binding.pry
     if @pay_streetaddress.valid? 
      
       @pay_streetaddress.save
-      
+    
       return redirect_to root_path
     else
       render :index
@@ -22,6 +22,6 @@ class PaysController < ApplicationController
   private
 
   def pay_params
-    params.permit(:postal_code, :area_id, :municipalities, :address, :tel).merge(item_id: @item.id , user_id: current_user.id)
+    params.require(:pay_street_address).permit(:postal_code, :area_id, :municipalities, :address, :tel).merge(item_id: @item.id , user_id: current_user.id)
   end
 end
