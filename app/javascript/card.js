f2ffcfd4724b4771508aca29236cac49d1bacb8b
mamewharu <mame0040007@gmail.com>
2021-02-20 16:52:29 +0900

    カード情報取得

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,17 @@
 const pay = () => {
+  // payjp.setPublickey()
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("イベント発火")
+    
+    const formResult = document.getElementById("charge-form");
+    const formDate = new FormData(formResult);
+    const card ={
+      number: formDate.get("pay_street_address[number]"),
+      cvc:  formDate.get("pay_street_address[cvc]"),
+      exp_month: formDate.get("pay_street_address[exp_month]"),
+      exp_year: `20${formDate.get("pay_street_address[exp_year]")}`,
+    };
   });
 };
 
